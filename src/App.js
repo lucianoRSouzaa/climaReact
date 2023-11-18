8f24e2823b56aac97f60b3ced41e453ffd97a2ad
Luciano Souza <lucianorodriguess101@gmail.com>
2023-11-18 16:43:15 -0300

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './index.css'
 
 import Header from './components/Header'
 import Main from './components/Main'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
     <main>
       <Header />
       <Main />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="d-flex flex-column align-items-center mb-3">
+            <p>Clima React - 2023</p>
+            <p>Desenvolvido por: Luciano Rodrigues De Souza</p>
+        </footer>
+    );
+}
